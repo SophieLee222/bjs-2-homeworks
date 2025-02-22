2418817155e2558d1d05d245ea8feedf178024bc
sofia va <sophia.lily.lee@gmail.com>
2025-02-22 23:22:09 +0300

    both array tasks completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,23 @@
 function compareArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else if (arr1.every((item, index) => item === arr2[index])) {
-    return true;
-  } else {
-    return false;
-  }
+	if (arr1.length !== arr2.length) {
+		return false;
+	} else if (arr1.every((item, index) => item === arr2[index])) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users
+		.filter(user => user.gender === gender)
+		.map(user => user.age)
+		.reduce((acc, age, index, arr) => {
+			acc += age;
+			if (index === arr.length - 1) {
+				return acc / arr.length;
+			}
+			return acc;
+		}, 0);
+	return result;
 }
