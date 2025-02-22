58e0aa8b73e4b1ccf672674112b96b7bcab2cae7
sofia va <sophia.lily.lee@gmail.com>
2025-02-22 22:09:11 +0300

    1st tast for arrays made

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,11 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else if (arr1.every((item, index) => item === arr2[index])) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
